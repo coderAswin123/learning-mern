eff5e6707d651b9169eb66e577eb9d2349941084
coderAswin123 <106464429+coderAswin123@users.noreply.github.com>
2025-03-21 01:20:49 +0530

    Revert "first pull request "

--- backend/routs/todo_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTodo, setTodo, updateTodo, deleteTodo } = require('../controllers/todo_controllers');
-
-router.route('/').get(getTodo).post(setTodo)
-router.route('/:id').put(updateTodo).delete(deleteTodo)
-
-module.exports = router;
