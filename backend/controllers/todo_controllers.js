0901c6fe0c555cdce979d8f81066c1fa2627eb23
Aswin VR <iitaspirant123@gmail.com>
2025-03-24 22:56:47 +0530

    added todomodels

--- backend/controllers/todo_controllers.js
+++ backend/controllers/todo_controllers.js
@@ -1,14 +1,20 @@
 const asyncHanlder = require('express-async-handler');
+
+const Todo = require('../models/todo_models');  
+
 const getTodo = asyncHanlder(async(request, response) => {
     response.set(200).json({message: 'Getting Todos', });
 })
 
-const setTodo = asyncHanlder(async(request, response) => {
+const setTodo = asyncHanlder(async (request, response) => {
     if(!request.body.text) {
         response.status(400);
         throw new Error('Please add a text in the response');
     }
-    response.set(200).json({message: 'Setting Todos'});
+    const todo = await Todo.create({
+        text : request.body.text,
+    });
+    response.set(200).json(todo);
 })
 
 const updateTodo = asyncHanlder(async(request, response) => {
