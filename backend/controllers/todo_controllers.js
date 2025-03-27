2fb071e00cb85084d0523fbe53579dfcb9dd3423
Aswin VR <iitaspirant123@gmail.com>
2025-03-27 22:25:16 +0530

    modified todocontroller

--- backend/controllers/todo_controllers.js
+++ backend/controllers/todo_controllers.js
@@ -1,60 +1,55 @@
-const asyncHanlder = require('express-async-handler');
+const asyncHandler = require('express-async-handler');
+const Todo = require('../models/todo_models');
 
-const Todo = require('../models/todo_models');  
-
-const getTodo = asyncHanlder(async(request, response) => {
+const getTodo = asyncHandler(async (request, response) => {
     const todo = await Todo.find();
-    response.set(200).json(todo);
-})
+    response.status(200).json(todo);
+});
 
-const setTodo = asyncHanlder(async (request, response) => {
-    if(!request.body.text) {
+const setTodo = asyncHandler(async (request, response) => {
+    if (!request.body.text) {
         response.status(400);
         throw new Error('Please add a text in the response');
     }
     const todo = await Todo.create({
-        text : request.body.text,
+        text: request.body.text,
     });
-    response.set(200).json(todo);
-})
+    response.status(200).json(todo);
+});
 
-const updateTodo = asyncHanlder(async(request, response) => {
+const updateTodo = asyncHandler(async (request, response) => {
+    const todo = await Todo.findById(request.params.id);
 
-    const todo =  await Todo.findByID(request.params.id);
-
-    if(!todo){
-        response.set(400);
+    if (!todo) {
+        response.status(400);
         throw new Error('Todo item not found');
     }
 
-    const updatedTodo = await Todo.findByIDAndUpdate(request.params.id, request.body, {
+    const updatedTodo = await Todo.findByIdAndUpdate(request.params.id, request.body, {
         new: true,
     });
 
-    response.set(200).json(updateTodo);
-})
+    response.status(200).json(updatedTodo);
+});
 
+const deleteTodo = asyncHandler(async (request, response) => {
+    const todo = await Todo.findById(request.params.id);
 
-const deleteTodo = asyncHanlder(async (request, response) => {
-    
-    const todo =  await Todo.findbyID(request.params.id);
-
-    if(!todo){
-        response.set(400);
+    if (!todo) {
+        response.status(400);
         throw new Error('Todo item not found');
     }
-    await Todo.findbyIdAndDelete(request.params.id);
+    await Todo.findByIdAndDelete(request.params.id);
 
-    response.set(200).json({
-
-        'message' : 'Deleting following id',
-        'id'      : request.params.id
+    response.status(200).json({
+        message: 'Deleting following id',
+        id: request.params.id
     });
-})
+});
 
 module.exports = {
     getTodo,
     setTodo,
     updateTodo,
     deleteTodo
-}
+};
