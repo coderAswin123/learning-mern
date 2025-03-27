105ef39af7964cbc907bad5e5bf3ab0325b25831
Aswin VR <iitaspirant123@gmail.com>
2025-03-27 19:59:07 +0530

    updated controller methods

--- backend/controllers/todo_controllers.js
+++ backend/controllers/todo_controllers.js
@@ -3,7 +3,8 @@ const asyncHanlder = require('express-async-handler');
 const Todo = require('../models/todo_models');  
 
 const getTodo = asyncHanlder(async(request, response) => {
-    response.set(200).json({message: 'Getting Todos', });
+    const todo = await Todo.find();
+    response.set(200).json(todo);
 })
 
 const setTodo = asyncHanlder(async (request, response) => {
@@ -18,12 +19,37 @@ const setTodo = asyncHanlder(async (request, response) => {
 })
 
 const updateTodo = asyncHanlder(async(request, response) => {
-    response.set(200).json({message: 'Updating Todos'});
+
+    const todo =  await Todo.findByID(request.params.id);
+
+    if(!todo){
+        response.set(400);
+        throw new Error('Todo item not found');
+    }
+
+    const updatedTodo = await Todo.findByIDAndUpdate(request.params.id, request.body, {
+        new: true,
+    });
+
+    response.set(200).json(updateTodo);
 })
 
 
 const deleteTodo = asyncHanlder(async (request, response) => {
-    response.set(200).json({message: 'Deleting Todos'});
+    
+    const todo =  await Todo.findbyID(request.params.id);
+
+    if(!todo){
+        response.set(400);
+        throw new Error('Todo item not found');
+    }
+    await Todo.findbyIdAndDelete(request.params.id);
+
+    response.set(200).json({
+
+        'message' : 'Deleting following id',
+        'id'      : request.params.id
+    });
 })
 
 module.exports = {
